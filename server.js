e1b0ba6852d406876622711a527bd1cb42ca4c14
Vaughn Aubrey <vaubrey7@gmail.com>
2022-10-24 17:19:30 -0600

    fixing Routes

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const PORT = process.env.PORT || 3001;
 // this is the middleware for the json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('publictacos'));
 
 //this is the middleware for the static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'publictacos')));
 
 // this is the middleware for the routes
-app.use('/api/taconotes', require('./routes/apiRoutes'));
-app.use('/', require('./routes/htmlRoutes'));
+app.use('/api/taconotes', require('./tacoroutes/apiRoutes'));
+app.use('/', require('./tacoroutes/htmlRoutes'));
 
 // this is the message you will recieve in your terminal 
 app.listen(PORT, () =>
